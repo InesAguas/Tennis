90c7bc824ee6ce47409332067ba98ea46843727e
InesAguas <inocas_99@hotmail.com>
2023-02-01 12:05:30 +0000

    Codigo pronto para fazer o pedido a cada 10 segundos

--- app/src/main/java/estgoh/tam/taniaines/tennis/activities/LiveGameActivity.java
+++ app/src/main/java/estgoh/tam/taniaines/tennis/activities/LiveGameActivity.java
@@ -3,9 +3,14 @@ package estgoh.tam.taniaines.tennis.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import estgoh.tam.taniaines.tennis.R;
 import estgoh.tam.taniaines.tennis.classes.Game;
@@ -13,6 +18,7 @@ import estgoh.tam.taniaines.tennis.classes.Game;
 public class LiveGameActivity extends AppCompatActivity {
 
     private TextView teste;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +30,29 @@ public class LiveGameActivity extends AppCompatActivity {
         Game game = (Game)b.getSerializable("game");
 
         teste = findViewById(R.id.teste);
+        handler = new Handler();
+        handler.postDelayed(runnable, 5000);
 
-        teste.setText(game.getStage());
-
+        //teste.setText(game.getStage());
 
         //mostrar informacoes jogo etc...
     }
 
-    private class requestUpdates extends AsyncTask<Void, Void, Void> {
-
+    private Runnable runnable = new Runnable() {
         @Override
-        protected Void doInBackground(Void... voids) {
-            return null;
+        public void run() {
+            Random rand = new Random();
+            teste.setText(rand.nextInt(20) + "");
+            /* and here comes the "trick" */
+            handler.postDelayed(this, 10000);
         }
+    };
+
+
+    public void showToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
+
+
+
 }
